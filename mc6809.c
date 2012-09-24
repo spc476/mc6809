ba44e87acff8f34ff63a01fe186287bfbc1dd666
Sean Conner <spc@conman.org>
2012-09-23 23:45:38 -0400

    Bug fix---more DAA adjustments
    The carry bit is set if there was a carry, *or* if it was set before the
    instruction was executed.
    
    This is some nasty stuff, but I think I finally have it done.

--- mc6809.c
+++ mc6809.c
@@ -429,7 +429,8 @@ int mc6809_step(mc6809__t *const cpu)
            mc6809byte__t msn = cpu->A >> 4;
            mc6809byte__t lsn = cpu->A & 0x0F;
            mc6809byte__t cf  = 0;
-           bool          hc  = cpu->cc.h;
+           bool          h   = cpu->cc.h;
+           bool          c   = cpu->cc.c;  
 
            if (cpu->cc.c || (msn > 9) || ((msn > 8) && (lsn > 9)))
              cf |= 0x60;      
@@ -437,7 +438,8 @@ int mc6809_step(mc6809__t *const cpu)
              cf |= 0x06;
              
            cpu->A    = op_add(cpu,cpu->A,cf);
-           cpu->cc.h = hc;
+           cpu->cc.h = h;
+           cpu->cc.c = cpu->cc.c || c;
          }
          break;
          
